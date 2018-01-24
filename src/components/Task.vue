57b1c02342572861f99798e2f75401cdff3e935b
Nicholas Litwin <nick.litwin@fundbox.com>
2018-01-24 09:32:44 -0800

    Create a task component

---
+++ src/components/Task.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .task-wrapper
+</template>
+
+<script>
+  import BtStopwatch from '@/components/Stopwatch'
+
+  export default {
+    name: 'bt-task',
+    components: {
+      BtStopwatch,
+    },
+    data: () => ({
+      // TODO what options are there for default prop values?
+      numTimesListened: null,
+    }),
+    methods: {
+      saveTaskData() {
+        // TODO: pass model to stopwatch to get time data?
+        alert(this.numTimesListened)
+      },
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
