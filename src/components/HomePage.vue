77bb904f752422e7cf8820f666b9171b51793f6e
Nicholas Litwin <nick.litwin@fundbox.com>
2018-01-23 21:07:23 -0800

    Create a HomePage route

---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .home-page-wrapper
+    bt-stopwatch
+</template>
+
+<script>
+  import BtStopwatch from '@/components/Stopwatch'
+
+  export default {
+    name: 'HomePage',
+    components: {
+      BtStopwatch,
+    },
+  }
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HomePage from '@/components/HomePage'
 
 Vue.use(Router)
 
@@ -7,5 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'HomePage',
+      component: HomePage,
+    },
   ]
 })
