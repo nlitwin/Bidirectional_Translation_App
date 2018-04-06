8ddf79964417447d735a780384dc808e938ecbf0
Nicholas Litwin <nick.litwin@fundbox.com>
2018-04-05 23:24:47 -0700

    Fix tests to see CI working

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
 
+Vue.use(Vuex)
 Vue.use(Router)
 
 export default new Router({
