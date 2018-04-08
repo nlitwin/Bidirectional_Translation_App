2708f058e68c20a66fa88de9c43db151d6894d92
Nicholas Litwin <nick.litwin@fundbox.com>
2018-04-08 14:59:54 -0700

    Finish basic task tests

--- cypress/integration/task_spec.js
+++ cypress/integration/task_spec.js
@@ -1,6 +1,9 @@
 describe('Task', function() {
-  beforeEach(function() {
+  before(function() {
     cy.visit('/')
+  })
+
+  beforeEach(function() {
     cy.fixture('task').as('taskData')
     cy.get('.task-wrapper').first().as('task')
   })
@@ -12,18 +15,29 @@ describe('Task', function() {
   })
 
   it('use the stopwatch', function() {
-    // TODO click the stopwatch and get value
+    cy.get('@task').find('[data-cy="stopwatch__toggle-btn"]')
+      .as('toggle')
+      .click()
+
+    cy.wait(2000)
+
+    cy.get('@toggle').click()
+
+    cy.get('@task').find('[data-cy="stopwatch__current-time"]')
+      // Be any single digit number, just not 00:00
+      .contains(/00:0[1-9]/)
   })
 
   it('enter notes', function() {
-    console.log(this)
     cy.get('@task').find('[data-cy="task__notes"]')
       .type(this.taskData.notes)
       .should('have.value', this.taskData.notes)
   })
 
   it('save your data', function() {
-    // TODO click the button
-    // TODO verify the button does something
+    cy.get('@task').find('[data-cy="task__save-btn"]')
+      .click()
+
+    // TODO: verify the button does something
   })
 })
